b5c339a1ff1a0ad38c52f6c1686908fa5823f5fe
marianotaha <marianoftaha@gmail.com>
2024-09-25 03:25:33 +0000

    auto commit 1       20240925 032523

--- src/wf-etapas/1301_FE_intrames_manual_005.r
+++ src/wf-etapas/1301_FE_intrames_manual_005.r
@@ -428,6 +428,29 @@ AgregarVariables_IntraMes <- function(dataset) {
     dataset[mapply(is.nan, dataset)] <<- 0
   }
   
+  
+  
+  # Aca agregamos los primeros 20 componentes principales que salen haciendo PCA
+  
+  
+  cat("Aca empieza a correr PCA. Suerte.\n")
+  
+  datasetsinNA <- dataset
+  datasetsinNA[is.na(datasetsinNA)] <- 0
+  pca_datos <- prcomp(datasetsinNA[,1:154],center=TRUE,scale=TRUE)
+  rm(datasetsinNA) # borramos este dataset creado para ahorrar espacio
+  autovec_pca <- as.data.table(pca_datos$x[,1:20])
+  rm(pca_datos) # borramos para ahorrar espacio
+  dataset <- dataset[, names(autovec_pca) := autovec_pca]
+  rm(autovec_pca) # borramos para ahorrar espacio
+  
+  cat("Si llegaste hasta aca es porque PCA no te hizo volar por los aires la corrida. Felicitaciones.\n")
+  
+  
+  
+  
+  
+  
   cat( "fin AgregarVariables_IntraMes()\n")
 }
 #------------------------------------------------------------------------------
