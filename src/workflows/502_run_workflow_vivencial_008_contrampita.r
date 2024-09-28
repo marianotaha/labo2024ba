b89186ce2f2b50b0a23c0101991a234ba3f8b68a
marianotaha <marianoftaha@gmail.com>
2024-09-28 05:52:46 +0000

    auto commit 2       20240928 055238

--- src/workflows/502_run_workflow_vivencial_008_contrampita.r
+++ src/workflows/502_run_workflow_vivencial_008_contrampita.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/z529_workflow_base_f202109_viv_008_contrampita.r"
+PARAM <- "src/workflows/529_workflow_base_f202109_viv_008_contrampita.r"
 
 envg <- env()
 
