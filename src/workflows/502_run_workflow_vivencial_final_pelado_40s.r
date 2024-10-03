c4a70b71887ef310ac9c1a60bf211f7a3875a00a
marianotaha <marianoftaha@gmail.com>
2024-10-03 03:40:43 +0000

    auto commit 2       20241003 034035

--- src/workflows/502_run_workflow_vivencial_final_pelado_40s.r
+++ src/workflows/502_run_workflow_vivencial_final_pelado_40s.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/z529_workflow_base_f202109_viv_final_pelado_40s.r"
+PARAM <- "src/workflows/529_workflow_base_f202109_viv_final_pelado_40s.r"
 
 envg <- env()
 
